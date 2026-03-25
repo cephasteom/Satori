0872bbdcaf5e7601b15e3c5251e171d553ca533a
Peter Thomas <petethomasmusique@gmail.com>
2026-03-25 11:55:41 +0000

    handle mutations in the ws handler

--- src/core/WebSocket.ts
+++ src/core/WebSocket.ts
@@ -1,4 +1,5 @@
 import { getDraw } from "tone";
+import { formatParamKey } from "../oto/utils";
 
 declare type Event = {id: string, params: Record<string, any>, time: number, type: string};
 
@@ -19,6 +20,38 @@ export function init() {
 export function handler(event: Event, time: number) {
     // if no WebSocket connection, ignore
     if(ws.readyState !== WebSocket.OPEN) return
-    console.log('Sending event to SuperSatori:', event);
-    getDraw().schedule(() => ws.send(JSON.stringify(event)), time);
+    switch (event.type) {
+        case 'e': return handleEvent(event, time);
+        case 'm': return handleMutation(event, time);
+    }
+}
+
+export function handleEvent(event: Event, time: number) {
+    const formattedEvent = {
+        ...event,
+        params: Object.entries(event.params).reduce((obj, [key, val]) => ({
+            ...obj,
+            // remove the _ prefix from all param keys as that's what the instruments expect
+            [formatParamKey(key)]: val
+        }), {})
+    }
+    // send event to SuperSatori via WebSocket
+    getDraw().schedule(() => ws.send(JSON.stringify(formattedEvent)), time);
+}
+
+export function handleMutation(event: Event, time: number) {
+
+    const formattedMutation = {
+        ...event,
+        params: Object.entries(event.params)
+            // only mutate params that are prefixed with '_'
+            .filter(([key, _]) => key.startsWith('_'))
+            // remove the _ prefix from all param keys as that's what the instruments expect
+            .reduce((obj, [key, val]) => ({
+                ...obj,
+                [formatParamKey(key)]: val
+            }), {})
+    }
+    // send mutation to SuperSatori via WebSocket
+    getDraw().schedule(() => ws.send(JSON.stringify(formattedMutation)), time);
 }
