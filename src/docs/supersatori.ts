453eb80cded94000eb9da7e8362e43b13afd73fc
Peter Thomas <petethomasmusique@gmail.com>
2026-05-10 07:51:49 +0100

    restyle: various tweaks

--- src/docs/supersatori.ts
+++ src/docs/supersatori.ts
@@ -1,5 +1,5 @@
 export default `
 SuperSatori is an alternative synth engine, built in SuperCollider. To use it:
-* Download SuperSatori from <TBC> and follow setup instructions in the README
+* Download SuperSatori from [https://github.com/cephasteom/supersatori](https://github.com/cephasteom/supersatori) and follow setup instructions in the README
 * Reload Satori, adding \`?engine=supersatori\` to the URL
 `
