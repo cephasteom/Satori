b923cebd7f1dba4080f714ae069bcbd38c31dab2
Peter Thomas <petethomasmusique@gmail.com>
2026-05-13 15:09:22 +0100

    fix preset example

--- src/editor/preset.ts
+++ src/editor/preset.ts
@@ -5,6 +5,7 @@ export const preset =
 s0.set({ 
   inst:'sampler', cut:'s0', bank:'breaks', 
   snap: ctms(8),
+  n: 60,
   begin: t(2).ifelse(random().step(1/16),0),
   dur: ctms(2),
   e: '1 1?0*2 | 1?0*4' })
