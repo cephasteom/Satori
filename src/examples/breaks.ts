c7fde9c850ce451356aeb8aa60f50c9b53f98e82
Peter Thomas <petethomasmusique@gmail.com>
2026-07-15 11:37:56 +0100

    update examples

--- src/examples/breaks.ts
+++ src/examples/breaks.ts
@@ -4,10 +4,8 @@ global.set({
   cps: 1.5, e: '1*4'})
 
 s0.set({ 
-  inst:'sampler', cut:'s0', bank:'breaks', 
-  snap: ctms(8),
-  n: 60,
+  inst:'sampler', bank:'breaks', cut: 0,
+  snap: ctms(8), 
   begin: t(2).ifelse(random().step(1/16),0),
-  dur: ctms(2),
   e: '1 1?0*2 | 1?0*4' })
 `
